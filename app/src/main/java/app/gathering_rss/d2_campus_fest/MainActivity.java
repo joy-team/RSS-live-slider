4670edd2c7a06c3e3989d1099c2eb34f06d2c2be
guswh11 <38255974+guswh11@users.noreply.github.com>
2019-07-13 23:09:06 +0900

    Update MainActivity.java
    connect viewPager to cardviewAdapter temporarily

--- app/src/main/java/app/gathering_rss/d2_campus_fest/MainActivity.java
+++ app/src/main/java/app/gathering_rss/d2_campus_fest/MainActivity.java
@@ -3,6 +3,7 @@ package app.gathering_rss.d2_campus_fest;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
@@ -14,6 +15,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,21 @@ public class MainActivity extends AppCompatActivity {
         tempList.add("1");
         tempList.add("2");
 
+        //recyclerview 임시 연결
         FeedAdapter feedAdapter = new FeedAdapter(MainActivity.this, tempList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         recyclerView.setAdapter(feedAdapter);
+
+        //viewpager 임시 연결
+        CardviewAdapter cardviewAdapter = new CardviewAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(cardviewAdapter);
+
+        for(int i=0;i<4; i++){
+            CardviewFragment cardviewFragment = new CardviewFragment();
+            cardviewAdapter.addItem(cardviewFragment);
+        }
+
+        cardviewAdapter.notifyDataSetChanged();
     }
 }
