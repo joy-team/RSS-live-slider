0ae597184a52b6f9efd404546f508de39a19bff0
L2zz <ulla4571@gmail.com>
2019-07-25 15:16:23 +0900

    Sample codes for video streaming

--- app/src/main/java/app/gathering_rss/d2_campus_fest/Constant.java
+++ app/src/main/java/app/gathering_rss/d2_campus_fest/Constant.java
@@ -7,4 +7,7 @@ public class Constant {
     public static final String ROSE = "5d2d56c48a93f8c5738b45675d36a65c8a93f8221d8b4567.xml";
     public static final String JISOO = "5d2d56c48a93f8c5738b45675d36a7098a93f8b9248b4567.xml";
     public static final String LISA = "5d2d56c48a93f8c5738b45675d36a75b8a93f80b2b8b4567.xml";
+
+    public static final String SAMPLE_VIDEO_URL =
+            "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4";
 }
